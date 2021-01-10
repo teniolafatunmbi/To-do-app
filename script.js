3990e7c5a11f01520cbcfb3ecf9ef14f69e42e2a
Omoteniola-dev <fatunmbiteniola@gmail.com>
2021-01-10 20:34:57 +0100

    Added the delete button and function added the crossOut function

--- script.js
+++ script.js
@@ -2,24 +2,38 @@ var button = document.getElementById("button");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
+
 //function to store input length
-const inputLength = () => {return input.value.length;
-}
+const inputLength = () => {return input.value.length;}
 
 //function to add items into the list
 const addItems = () => {
-	var button1 = document.createElement("button");
 	var li = document.createElement("li"); //creates a list element<li>
-	li.appendChild(document.createTextNode(input.value + button1.value));//add the inputs to the list element
+	li.appendChild(document.createTextNode(input.value));//add the inputs to the list element
 	ul.appendChild(li); //adds the list element to the list of to-dos
-	button1.value = "x";
 	input.value = ""; //sets the input value to null
+
+	const crossOut = () => {
+		li.classList.toggle("done");
+	}
+	li.addEventListener("click", crossOut);
+
+	const deleteListItem = () => {
+		li.classList.add("delete");
+	}
+
+	//add delete button
+	var button1 = document.createElement("button");
+	button1.appendChild(document.createTextNode("X"));
+	li.appendChild(button1);
+	button1.addEventListener("click", deleteListItem);
+	//END ADD DELETE BUTTON
+
+	
 }
 
-//function to remove elements from the to-do
-const removeItems = () => {
-	ul.appendChild(li + button1);
-}
+
+
 
 //function to add items to the to-do
 const addItemsAfterClick = () => {
